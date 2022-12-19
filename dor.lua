bbd9b0528a3baaec3ee8e8bc0da23f5cd18da51e
DXuwu <83565019+DXuwu@users.noreply.github.com>
2022-12-19 21:00:59 +0800

    Update dor.lua

--- dor.lua
+++ dor.lua
@@ -1731,7 +1731,7 @@ end}
 local selectedTool=Tools:CreateDropdown({
     Name="选择物品",
     Options=toolList,
-    CurrentOption="Crucifix",
+    CurrentOption="十字架",
     Flag="selectedTool",
     Callback=function() end
 })
@@ -2225,6 +2225,100 @@ MainTab:CreateDropdown({
         SelectedDoorsEntity=Option
 	end,
 })
+
+MainTab:CreateButton({
+        Name = "A-60",
+        Callback = function()
+            local Creator = loadstring(game:HttpGet("https://pastebin.com/raw/txV1ZG7S"))() 
+local entity = Creator.createEntity({
+    CustomName = "A-60", -- Custom name of your entity
+    Model = "rbxassetid:////11835351318", -- Can be GitHub file or rbxassetid
+    Speed = 700, -- Percentage, 100 = default Rush speed
+    DelayTime = 2, -- Time before starting cycles (seconds)
+    HeightOffset = 0,
+    CanKill = false,
+    KillRange = 40,
+    BreakLights = true,
+    BackwardsMovement = false,
+    FlickerLights = {
+        true, -- Enabled/Disabled
+        1, -- Time (seconds)
+    },
+    Cycles = {
+        Min = 7,
+        Max = 7,
+        WaitTime = 1,
+    },
+    CamShake = {
+        true, -- Enabled/Disabled
+        {3.5, 20, 0.1, 1}, -- Shake values (don't change if you don't know)
+        100, -- Shake start distance (from Entity to you)
+    },
+    Jumpscare = {
+        true, -- Enabled/Disabled
+        {
+            Image1 = "rbxassetid://10483855823", -- Image1 url
+            Image2 = "rbxassetid://10483999903", -- Image2 url
+            Shake = true,
+            Sound1 = {
+                10483790459, -- SoundId
+                { Volume = 0.5 }, -- Sound properties
+            },
+            Sound2 = {
+                10483837590, -- SoundId
+                { Volume = 0.5 }, -- Sound properties
+            },
+            Flashing = {
+                true, -- Enabled/Disabled
+                Color3.fromRGB(0, 0, 255), -- Color
+            },
+            Tease = {
+                true, -- Enabled/Disabled
+                Min = 4,
+                Max = 4,
+            },
+        },
+    },
+    CustomDialog = {"你死于A-60...", "等一下", "鸡"}, -- Custom death message
+    
+})
+
+-----[[ Advanced ]]-----
+entity.Debug.OnEntitySpawned = function(entityTable)
+    print("Entity has spawned:", entityTable.Model)
+end
+
+entity.Debug.OnEntityDespawned = function(entityTable)
+    print("Entity has despawned:", entityTable.Model)
+end
+
+entity.Debug.OnEntityStartMoving = function(entityTable)
+    print("Entity has started moving:", entityTable.Model)
+end
+
+entity.Debug.OnEntityFinishedRebound = function(entityTable)
+    print("Entity has finished rebound:", entityTable.Model)
+end
+
+entity.Debug.OnEntityEnteredRoom = function(entityTable, room)
+    print("Entity:", entityTable.Model, "has entered room:", room)
+end
+
+entity.Debug.OnLookAtEntity = function(entityTable)
+    print("Player has looked at entity:", entityTable.Model)
+end
+
+entity.Debug.OnDeath = function(entityTable)
+    warn("Player has died.")
+end
+------------------------
+
+-- Run the created entity
+Creator.runEntity(entity)
+end,
+})
+
+
 MainTab:CreateSection("自定义怪物")
 MainTab:CreateDropdown({
 	Name = "选择自定义怪物",
@@ -2327,4 +2421,3 @@ MainTab:CreateInput({
 })
 --#endregion
 --new region
-Rayfield:LoadConfiguration()
